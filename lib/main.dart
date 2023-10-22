76b73710cae4327e0f15cfaa6b3bba2d4d170d86
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-10-22 02:18:28 +0100

    make scroll view and add safe area

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,33 @@ class MyScrollView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          floating: true,
-          title: Text("Sticky Headers")
-        )
-      ],
+    return SafeArea(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          const SliverAppBar(
+            floating: true,
+            title: Text("Sticky Headers"), centerTitle: true,
+          ),
+    
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Column(
+                  children: <Widget>[
+                    Container(
+                      height: 50,
+                      color: Colors.grey,
+                      alignment: Alignment.centerLeft,
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Text('Section $index', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                    )
+                  ],
+                );
+              }
+            ),
+          )
+        ],
+      ),
     );
   }
 }

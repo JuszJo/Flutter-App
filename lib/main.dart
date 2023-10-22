c03471f8a47f1b8ae21c0e00c1b46d5bb1c5aa00
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-10-22 02:24:50 +0100

    add sub list

--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,45 @@ class MyScrollView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: CustomScrollView(
-        slivers: <Widget>[
-          const SliverAppBar(
-            floating: true,
-            title: Text("Sticky Headers"), centerTitle: true,
-          ),
-    
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Column(
-                  children: <Widget>[
-                    Container(
-                      height: 50,
-                      color: Colors.grey,
-                      alignment: Alignment.centerLeft,
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Text('Section $index', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                    )
-                  ],
-                );
-              }
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: <Widget>[
+            const SliverAppBar(
+              floating: true,
+              title: Text("Sticky Headers"), centerTitle: true,
             ),
-          )
-        ],
+          
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Column(
+                    children: <Widget>[
+                      Container(
+                        height: 50,
+                        color: Colors.grey,
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Text('Section $index', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                      ),
+      
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: 10,
+                        itemBuilder: (BuildContext context, int subIndex) {
+                          return ListTile(
+                            title: Text('Item $subIndex in section $index'),
+                          );
+                        },
+                      )
+                    ],
+                  );
+                }
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
